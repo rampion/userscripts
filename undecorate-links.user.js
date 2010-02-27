0c53fd73ed1d98386edf4010db71d27e65c338e4
Noah Luck Easterly <noah.easterly@gmail.com>
2010-02-27 00:42:10 -0500

    switched to using mouseevents to detect links, so I could modify links that were added to the page post-load

--- undecorate-links.user.js
+++ undecorate-links.user.js
@@ -5,8 +5,8 @@
 // @include https://www.rci.com*
 // @include http://www.rci.com*
 // ==/UserScript==
-const as = document.getElementsByTagName('a');
-for (var i = 0; i < as.length; ++i) (function(a){
+
+const undecorate_link = function(a){
   if (!a.href.match(/^javascript:/)) return;
 	var match;
  
@@ -14,5 +14,18 @@ for (var i = 0; i < as.length; ++i) (function(a){
     a.href = 'http://www.rciaffiliates.com';
   } else if (match = a.href.match(/^javascript:(?:newPopWindow|getLink)\((['"])((?:(?!\1)[^\\]|\\.)*)\1.*?\);?$/)) {
     a.href = match[2];
-  } 
-})(as[i]);
+	}
+}; 
+
+//const as = document.getElementsByTagName('a');
+//for (var i = 0; i < as.length; ++i) undecorate_link(as[i]);
+
+document.addEventListener('mouseover', function(mouseover_event){
+		if (mouseover_event.target.tagName == 'A') { undecorate_link(mouseover_event.target); }
+}, false);
+document.addEventListener('mouseout', function(mouseout_event){
+		if (mouseout_event.target.tagName == 'A') { undecorate_link(mouseout_event.target); }
+}, false);
+document.addEventListener('click', function(click_event){
+		if (click_event.target.tagName == 'A') { undecorate_link(click_event.target); }
+}, false);

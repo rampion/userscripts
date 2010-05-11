c4deb4b26c62f19e981ad8ec3e4d495efbb1faca
Noah Luck Easterly <noah.easterly@gmail.com>
2010-05-10 23:33:45 -0400

    a couple bug fixes

--- TwitterReplyToPreview.user.js
+++ TwitterReplyToPreview.user.js
@@ -29,25 +29,25 @@ const set_titles = function(){
 		}
 	})(spans[i]);
 };
-// todo: watch for page updates and set titles on new tweets
-set_titles();
 
-const timeline = document.getElementById('timeline');
 var nStatuses = 0;
 
 const onclick = function(click_event){ 
 	// the button's about to go away
 	if (click_event) click_event.target.removeEventListener('click', onclick, false);
 
+	const timeline = document.getElementById('timeline');
+
 	// wait for new statuses to be loaded
-	if (nStatuses == timeline.childNodes.length) {
-		setTimeout(onclick, 500, null);
-	} else {
-		nStatuses = timeline.childNodes.length;
-		// add new status titles
-		set_titles();
-		// look for more when more are requested
-		document.getElementById('more').addEventListener('click', onclick, false);
-	}
+	if (!timeline || nStatuses == timeline.childNodes.length) return setTimeout(onclick, 500, null);
+
+	nStatuses = timeline.childNodes.length;
+
+	// add new status titles
+	set_titles();
+
+	// look for more when more are requested
+	document.getElementById('more').addEventListener('click', onclick, false);
 };
+
 onclick(null);
